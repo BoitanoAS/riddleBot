07cc8abc9b8fb98a31b57be3a2ea835a045f5c47
maur1 <maureen@live.no>
2023-03-26 12:48:52 +0200

    update leadboard to blocks

--- getScoreBoard/lambda_function.py
+++ getScoreBoard/lambda_function.py
@@ -3,7 +3,10 @@ from slack_sdk import WebClient
 import os
 import base64
 import json
+import logging
 
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
 def get_secret(secret_name):
     client = boto3.client(service_name='secretsmanager')
@@ -12,33 +15,59 @@ def get_secret(secret_name):
 
 
 def return_text(slack_client, resp_txt):
+    logging.info(f"responded w {resp_txt}")
     slack_client.chat_postMessage(
         channel=os.getenv("CHANNEL_ID"),
-        text=f"``` \n {resp_txt} ```")
-    print(f"responded w {resp_txt}")
+        text="Poengtavle",
+        blocks = [{
+            "type": "section",
+            "fields":resp_txt
+        }])
+
 
 
 def generate_leaderboard(user_dct):
-    board = "{:<40} {:<40}\n".format('*Navn*', '*Poeng*')
-    board += "-" * 50 + "\n"
+    user_dct = sorted(user_dct, key=lambda d: d['poeng'], reverse=True)
+    pnt_str = ""
+    name_str = ""
     for usr in user_dct:
-        board += "  {:<40} {:<40}\n".format(usr.get("navn"), usr.get("poeng"))
-    return board
+        pnt_str += str(usr.get("poeng"))+'\n'
+        name_str += usr.get("navn")+'\n'
+    point_tbl = [
+        {
+            "type": "mrkdwn",
+            "text": "*Navn*"
+        },
+        {
+            "type": "mrkdwn",
+            "text": "*Poeng*"
+        },
+        {
+            "type": "plain_text",
+            "text": name_str,
+        },
+        {
+            "type": "plain_text",
+            "text": pnt_str,
+        }
+    ]
+    return point_tbl
+
 
 
 def get_user_id_from(body):
-    decoded_bd = base64.b64decode(body).decode("ascii").replace("%22", '"').replace("%3A", ":").replace("%2C",
-                                                                                                        ",").replace(
-        "%7B", "{").replace("%7D", "}").replace("payload=", "")
+    decoded_bd = base64.b64decode(body).decode("ascii").replace("%22", '"').replace("%3A", ":").replace("%2C", ",").replace("%7B", "{").replace("%7D", "}").replace("payload=", "")
     bd_dct = json.loads(decoded_bd)
     return bd_dct.get("user").get("id")
 
 
 def lambda_handler(event, _):
-    admins = ["U04RU0NTNGG", "U04RR7JBZ0V", "U04RMH9SPV4"]
+    logging.info(event)
+    admins = ["U044549TA5N", "UALUFB8UW", "UAQ1A0WBS"]
     user_id = get_user_id_from(event.get("body"))
+    logging.info(user_id)
     if user_id in admins:
-        print("Received leaderboard event")
+        logger.info("Received leaderboard event")
         boto3.setup_default_session(region_name=os.getenv("REGION"))
         dbd_client = boto3.resource('dynamodb')
         board_tbl = dbd_client.Table(os.getenv("BOARD_TBL"))
